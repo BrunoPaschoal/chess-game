b1eba06fc0713c3ec7f7ea3fbd9835bd92651190
Bruno Rocha <bruno.rocha.dev@outlook.com>
2020-12-30 01:29:51 -0300

    Criando a classe posição

--- Program.cs
+++ Program.cs
@@ -1,9 +1,50 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3,4);
+
+            Console.WriteLine($"Posição: {P}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
+
+
+
+
+
+//ANOTAÇÕES
+
+/* O projeto seguirá uma arquitetura de camadas, A primeira camada básica será
+ a camda do tabuleiro, onde terá as coisas básicas do tabuleiro (Peças, tabuleirro, posição, cor da peça)
+
+ Acima dessa camada, terá a camada do jogo de xadrez, onde serão implementadas as regras do jogo.
+
+ A camada que está mais a cima, a camada de aplicação, é a camada que vai consumir as duas de baixo e executar o jogo*/
---
+++ tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿
+
+namespace tabuleiro {
+    class Posicao {
+        //Criando atributos da posição
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        //CONSTUTORES
+        public Posicao(int linha, int coluna) {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        //MÉTODOS
+        //Método para converter para string quando o objeto instanciado por exibido na tela
+        public override string ToString() {
+            return Linha
+                + ", "
+                + Coluna;
+        }
+    }
+}
