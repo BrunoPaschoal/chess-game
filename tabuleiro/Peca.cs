f3a4b76f57238ba57afa247d7ff3989fcefa8dfc
Bruno Rocha <bruno.rocha.dev@outlook.com>
2020-12-30 01:54:42 -0300

    Començando a implementar o tabuleiro

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace tabuleiro {
+    class Peca {
+        //Atributo posição está associada com a classe procisão, assim como a cor
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; } //Atributo pode ser modificada só pela mesma classe e pelas subclasses dela
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        //CONSTRUTORES
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor) {
+            Posicao = posicao;
+            Cor = cor;
+            Tab = tab;
+            //Quantidade de movimento, no início do jogo, será iniciado com zero
+            QtdMovimentos = 0;
+        }
+    }
+}
---
+++ tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro {
+    class Tabuleiro {
+        //Um tabuleiro tem um número de linhas e um número de colunas
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        //Um xadrez contém uma matriz do tipo Peça
+        private Peca[,] pecas;
+
+        //CONSTRUTOR
+        public Tabuleiro(int linhas, int colunas) {
+            Linhas = linhas;
+            Colunas = colunas;
+            //As peças receberão uma nova matriz de peças, com número de linhas e colunas
+            pecas = new Peca[linhas, colunas];
+        }
+        
+    }
+}
