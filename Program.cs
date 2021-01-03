5f05096352b2682ef37f745c4b9ad83324ee683a
Bruno Rocha <bruno.rocha.dev@outlook.com>
2021-01-03 00:21:54 -0300

    Criando a classe posição xadrez

--- Program.cs
+++ Program.cs
@@ -6,19 +6,11 @@ namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
 
-            try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                //Colocando peças no tabuleiro
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 8));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-                //Chamando método para imprimir o tabuleiro
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch(TabuleiroException e) {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.toPosicao());
 
         }
     }
---
+++ xadrez/PosicaoXadrez.cs
@@ -0,0 +1,25 @@
+﻿using tabuleiro; 
+
+namespace xadrez {
+    class PosicaoXadrez {
+        //cada casinha do xadrez é representada por um endereço de uma letra e um número inteiro
+        //Os atributos dessa classe irão reprensetar isso
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        //Construtor
+        public PosicaoXadrez(char coluna, int linha) {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        //Método que converte a posição da casa para posição da matriz
+        public Posicao toPosicao() {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString() {
+            return "" + Coluna + Linha;
+        }
+    }
+}
